24dd319902ffb70e3fe187b696222f50983d929a
Suprim1043 <Suprim1043@gmail.com>
2025-01-10 22:17:25 -0600

    React and Mantine setup with Vite

---
+++ reactMantine/react-ui/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button, Title } from '@mantine/core';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <Title order={2}>Welcome to Mantine with Vite</Title>
+      <Button>Hello Mantine!</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactMantine/react-ui/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { MantineProvider } from '@mantine/core';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <MantineProvider theme={{ colorScheme: 'light' }}>
+    <App />
+  </MantineProvider>
+);
